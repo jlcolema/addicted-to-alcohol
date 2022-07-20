5f0a478275f4f215d3b1dd189cf2730c6a8f7cdb
Joshua Coleman <4628855+jlcolema@users.noreply.github.com>
2022-07-20 08:37:14 -0500

    Add waves to hero.

--- app/public/wp-content/themes/addictedtoalcohol/page-templates/content-modules.php
+++ app/public/wp-content/themes/addictedtoalcohol/page-templates/content-modules.php
@@ -25,20 +25,35 @@ get_header(); ?>
 			?>
 
 			<?php if($layout == 'Layout One') : ?>
-				<div class="hero" style="background-image:url(<?php echo $bg_url ? $bg_url : $default_thumb; ?>);">
-					<div class="container">
+				<div class="hero" <?php if ( $image ) : ?>style="background-image:url(<?php echo $bg_url ? $bg_url : $default_thumb; ?>);"<?php endif; ?>>
+					<div class="hero__wave hero__wave--upper">
+						<svg class="hero__svg hero__svg--upper" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+							<path class="elementor-shape-fill" opacity="0.33" d="M473,67.3c-203.9,88.3-263.1-34-320.3,0C66,119.1,0,59.7,0,59.7V0h1000v59.7 c0,0-62.1,26.1-94.9,29.3c-32.8,3.3-62.8-12.3-75.8-22.1C806,49.6,745.3,8.7,694.9,4.7S492.4,59,473,67.3z"></path>
+							<path class="elementor-shape-fill" opacity="0.66" d="M734,67.3c-45.5,0-77.2-23.2-129.1-39.1c-28.6-8.7-150.3-10.1-254,39.1 s-91.7-34.4-149.2,0C115.7,118.3,0,39.8,0,39.8V0h1000v36.5c0,0-28.2-18.5-92.1-18.5C810.2,18.1,775.7,67.3,734,67.3z"></path>
+							<path class="elementor-shape-fill" d="M766.1,28.9c-200-57.5-266,65.5-395.1,19.5C242,1.8,242,5.4,184.8,20.6C128,35.8,132.3,44.9,89.9,52.5C28.6,63.7,0,0,0,0 h1000c0,0-9.9,40.9-83.6,48.1S829.6,47,766.1,28.9z"></path>
+						</svg>
+
+					</div>
+					<div class="container hero__container">
 						<?php if($heading) : ?>
-							<div class="pre-heading"><?php echo $heading; ?></div>
+							<div class="pre-heading hero__pre-heading"><?php echo $heading; ?></div>
 						<?php endif; ?>
 						<?php if($bold_heading) : ?>
 							<h1><?php echo $bold_heading; ?></h1>
 						<?php endif; ?>
 						<?php if($subheading) : ?>
-							<p><?php echo $subheading; ?></p>
+							<p class="hero__subheading"><?php echo $subheading; ?></p>
 						<?php endif; ?>
 						<?php if($button) : ?>
-							<a class="btn btn-secondary btn-lg" target="<?php echo $button['target']; ?>" href="<?php echo esc_url($button['url']); ?>"><?php echo esc_html($button['title']); ?> <?php if($add_phone_icon_to_button) : ?><i class="fas fa-phone"></i><?php endif; ?></a>
+							<a class="btn btn-secondary btn-lg hero__button" target="<?php echo $button['target']; ?>" href="<?php echo esc_url($button['url']); ?>"><?php echo esc_html($button['title']); ?> <?php if($add_phone_icon_to_button) : ?><i class="fas fa-phone"></i><?php endif; ?></a>
 						<?php endif; ?>
+					</div>
+					<div class="hero__wave hero__wave--lower">
+						<svg class="hero__svg hero__svg--lower" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+							<path class="elementor-shape-fill" opacity="0.33" d="M473,67.3c-203.9,88.3-263.1-34-320.3,0C66,119.1,0,59.7,0,59.7V0h1000v59.7 c0,0-62.1,26.1-94.9,29.3c-32.8,3.3-62.8-12.3-75.8-22.1C806,49.6,745.3,8.7,694.9,4.7S492.4,59,473,67.3z"></path>
+							<path class="elementor-shape-fill" opacity="0.66" d="M734,67.3c-45.5,0-77.2-23.2-129.1-39.1c-28.6-8.7-150.3-10.1-254,39.1 s-91.7-34.4-149.2,0C115.7,118.3,0,39.8,0,39.8V0h1000v36.5c0,0-28.2-18.5-92.1-18.5C810.2,18.1,775.7,67.3,734,67.3z"></path>
+							<path class="elementor-shape-fill" d="M766.1,28.9c-200-57.5-266,65.5-395.1,19.5C242,1.8,242,5.4,184.8,20.6C128,35.8,132.3,44.9,89.9,52.5C28.6,63.7,0,0,0,0 h1000c0,0-9.9,40.9-83.6,48.1S829.6,47,766.1,28.9z"></path>
+						</svg>
 					</div>
 				</div>
 			<?php elseif($layout == 'Layout Three') : ?>
