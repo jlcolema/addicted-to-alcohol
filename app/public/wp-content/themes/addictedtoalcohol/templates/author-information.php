bb54eb2c8d8ad72545e255024dbb43570a9648ff
Joshua Coleman <4628855+jlcolema@users.noreply.github.com>
2022-07-21 20:21:41 +0000

    Change Author Information component to match project.

--- app/public/wp-content/themes/addictedtoalcohol/templates/author-information.php
+++ app/public/wp-content/themes/addictedtoalcohol/templates/author-information.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 	$published_date = get_field('author_information');
 ?>
 
 <div class="additional-info">
 	<div class="row">
 		<div class="col-12 col-md-6">
-				<p><span style="font-size:smaller">Written by</span><br>Ohio Recovery Center Editorial Team</p>
+				<p><span style="font-size:smaller">Written by</span><br>Addicted to Alcohol Editorial Staff</p>
 				<?php include(get_template_directory().'/templates/date-options.php'); ?>
-				<p class="small">&copy;<?php echo date('Y');?> Ohio Recovery Center | All Rights Reserved</p>
+				<p class="small">&copy;<?php echo date('Y');?> Addicted to Alcohol | All Rights Reserved</p>
 		</div>
 		<div class="col-12 col-md-6 text-right">
 			<p><u>This page does not provide medical advice.</u></p>
