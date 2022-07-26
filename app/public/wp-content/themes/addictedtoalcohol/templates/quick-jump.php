4675910d4e8287a483b6dcf3bacf01a90ed005e5
Joshua Coleman <4628855+jlcolema@users.noreply.github.com>
2022-07-26 09:33:37 -0500

    Add classes to quick-jump component.

--- app/public/wp-content/themes/addictedtoalcohol/templates/quick-jump.php
+++ app/public/wp-content/themes/addictedtoalcohol/templates/quick-jump.php
@@ -1,7 +1,11 @@
 <?php if(have_rows('quick_jump')) : ?>
-	<ul class="page-anchors">
-		<?php while(have_rows('quick_jump')) : the_row(); ?>
-			<li><a href="#<?php the_sub_field('anchor_id'); ?>"><?php the_sub_field('anchor_text'); ?></a></li>
-		<?php endwhile; ?>
-	</ul>
+	<div class="quick-jump">
+		<ul id="" class="page-anchors quick-jump__list">
+			<?php while(have_rows('quick_jump')) : the_row(); ?>
+				<li class="quick-jump__item">
+					<a href="#<?php the_sub_field('anchor_id'); ?>" class="quick-jump__link"><?php the_sub_field('anchor_text'); ?></a>
+				</li>
+			<?php endwhile; ?>
+		</ul>
+	</div>
 <?php endif; ?>
